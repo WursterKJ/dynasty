8cf12c5257d66f670772c1584e285dff55e8f6d6
WursterKJ <kylewurster28@gmail.com>
2026-07-14 21:20:12 -0500

    Complete Players Data Pull

--- code/players_data.py
+++ code/players_data.py
@@ -10,11 +10,15 @@ import os
 # sleeper data is giant nested json with player_id as key and large list of keys within as the pair
 # cannot do direct df because of the dict key pairs (dict data type)
 # need to un nest the inner key pairs (.json() command creates a dict) into new dict key pairs with column matches
+
 players_url = "https://api.sleeper.app/v1/players/nfl"
 players_data = requests.get(players_url).json()
 # takes newly converted data (source json to dict and makes data frame from dict)
 players_df = pd.DataFrame.from_dict(players_data, orient="index")
-print(players_df)
-#players_data.to_csv("data/raw/players.csv", index = False)
-#print("Successfully Pulled Player Data")
+players_filtered = players_df.query("active == True and status == 'Active' and (position == 'QB' or position == 'RB' or position == 'WR' or position == 'TE')")
+players_filtered_columns = players_filtered.filter(items = ["player_id", "full_name", "position", "team", "number", "college", "years_exp", "birth_date", "age", "height", "weight", "depth_chart_order", "injury_status", "injury_body_part", "stats_id", "fantasy_data_id"])
+# orient index flips columns and rows using indices as columns
+# print(players_filtered)
+players_filtered_columns.to_csv("data/raw/players.csv", index = False)
+print("Successfully Pulled Player Data")
 
