e98c1c2fa384bd98bffb93a38b76530b493cd3e1
WursterKJ <kylewurster28@gmail.com>
2026-07-25 01:10:31 -0500

    Continue with Contracts

--- scripts/contracts_data.py
+++ scripts/contracts_data.py
@@ -3,14 +3,15 @@ import nflreadpy as nfl
 
 def contracts_refresh():
     master_data = pd.read_csv("data/master.csv")
-    master_list = master_data["player_id"].astype(str).to_list()
+    master_list = master_data["search_full_name"].astype(str).to_list()
 
-    ids_data = nfl.load_ff_playerids().to_pandas()
-    sleeper_ids = ids_data.filter(items=["sleeper_id"]).astype("Int64")
-    sleeper_ids = sleeper_ids.astype(str).query("sleeper_id in @master_list")
+    search_data = nfl.load_ff_playerids().to_pandas()
+    player_search = search_data.filter(items=["merge_name"]).astype(str)
+    # must specify column to not chaneg df to series, adjust just inner series of df not full df
+    player_search["merge_name"] = player_search["merge_name"].str.replace(" ", "")
+    player_search = player_search.query("merge_name in @master_list")
 
     contracts_data = nfl.load_contracts().to_pandas()
-    contracts_data = sleeper_ids.join(contracts_data, how="left").query("is_active == True").filter(items=["sleeper_id", "year_signed", "years", "apy"])
+    contracts_data = player_search.join(contracts_data, how="left").query("is_active == True").filter(items=["merge_name", "year_signed", "years", "apy"])
 
     return contracts_data
-
