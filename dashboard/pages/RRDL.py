ca92ffc21489e3e51a8d1f30dbf5f2e5ed209b90
WursterKJ <kylewurster28@gmail.com>
2026-08-04 23:48:37 -0500

    Create Averages and Ranks

--- dashboard/pages/RRDL.py
+++ dashboard/pages/RRDL.py
@@ -33,24 +33,35 @@ user_select_df = master.query("display_name_freedom == @user_select")
 user_select_df = user_select_df.merge(stats, how="left", on="player_id").query("season == @stat_season")
 user_select_df["position"] = user_select_df["position"].astype(position_priority)
 
-user_select_qb = user_select_df.query("position == 'QB'")
-user_select_rb = user_select_df.query("position == 'RB'")
-user_select_wr = user_select_df.query("position == 'WR'")
-user_select_te = user_select_df.query("position == 'TE'")
+season_df = master.merge(stats, how="left", on="player_id").query("season == @stat_season")
+season_df["position"] = season_df["position"].astype(position_priority)
+
+team_pivot = season_df.pivot_table(index="display_name_freedom", columns="position", values="ppg_freedom", aggfunc="mean").reset_index()
+team_stats = season_df.groupby(["display_name_freedom"], as_index=False).agg(tot_pts=("points_freedom", "sum"), tot_per_player=("points_freedom", "mean"), ppg_player=("ppg_freedom", "mean"))
+team_stats = team_stats.merge(team_pivot, how="left", on="display_name_freedom").rename(columns={"QB":"ppg_qb", "RB":"ppg_rb", "WR":"ppg_wr", "TE":"ppg_te"})
+team_stats[["rank_tot_pts", "rank_tot_per_player", "rank_ppg_player", "rank_ppg_qb", "rank_ppg_rb", "rank_ppg_wr", "rank_ppg_te" ]] = team_stats[["tot_pts", "tot_per_player", "ppg_player", "ppg_qb", "ppg_rb", "ppg_wr", "ppg_te"]].rank(method="min", ascending=False)
+team_stats_user = team_stats.query("display_name_freedom == @user_select")
 
 avg_age = round(user_select_df["age"].mean(), 1)
 avg_apy = round(user_select_df["apy"].mean(), 2)
 avg_depth = round(user_select_df["depth_chart_order"].mean(), 1)
 avg_expire = round(user_select_df["year_final"].mean(), 1)
 
-tot_pts_max = round(user_select_df["points_freedom"].sum(), 1)
-avg_pts_max = round((user_select_df["points_freedom"].sum())/17, 1)
-
-avg_ppg = round(user_select_df["ppg_freedom"].mean(), 1)
-avg_ppg_qb = round(user_select_qb["ppg_freedom"].mean(), 1)
-avg_ppg_rb = round(user_select_rb["ppg_freedom"].mean(), 1)
-avg_ppg_wr = round(user_select_wr["ppg_freedom"].mean(), 1)
-avg_ppg_te = round(user_select_te["ppg_freedom"].mean(), 1)
+# take first record of column (always only one row beside headers)
+tot_pts = round(team_stats_user["tot_pts"].iloc[0], 1)
+tot_per_player = round(team_stats_user["tot_per_player"].iloc[0], 1)
+ppg_player = tot_pts = round(team_stats_user["ppg_player"].iloc[0], 1)
+ppg_qb = round(team_stats_user["ppg_qb"].iloc[0], 1)
+ppg_rb = round(team_stats_user["ppg_rb"].iloc[0], 1)
+ppg_wr = round(team_stats_user["ppg_wr"].iloc[0], 1)
+ppg_te = round(team_stats_user["ppg_te"].iloc[0], 1)
+rank_tot_pts = int(team_stats_user["rank_tot_pts"].iloc[0])
+rank_tot_per_player = int(team_stats_user["rank_tot_per_player"].iloc[0])
+rank_ppg_player = int(team_stats_user["rank_ppg_player"].iloc[0])
+rank_ppg_qb = int(team_stats_user["rank_ppg_qb"].iloc[0])
+rank_ppg_rb = int(team_stats_user["rank_ppg_rb"].iloc[0])
+rank_ppg_wr = int(team_stats_user["rank_ppg_wr"].iloc[0])
+rank_ppg_te = int(team_stats_user["rank_ppg_te"].iloc[0])
 
 count_pos = user_select_df.value_counts("position").reset_index().sort_values("position")
 count_pos_bar = px.bar(count_pos, x="position", y="count", color_discrete_sequence=[primary])
@@ -74,11 +85,20 @@ if focus_select == "Roster":
 else:
     st.header(user_select)
     st.subheader(focus_select)
-    st.metric("Points:", tot_pts_max)
-    st.metric("Points per:", avg_pts_max)
-    st.metric("PPG:", avg_ppg)
-    st.metric("QB:", avg_ppg_qb)
-    st.metric("RB:", avg_ppg_rb)
-    st.metric("WR:", avg_ppg_wr)
-    st.metric("TE:", avg_ppg_te)
-    st.dataframe(stat_table, hide_index=True)
+    topcol1, topcol2, topcol3 = st.columns(3)
+    with topcol1:
+        st.metric("Points:", tot_pts, rank_tot_pts, "off")
+    with topcol2:
+        st.metric("Points per:", tot_per_player, rank_tot_per_player, "off")
+    with topcol3:
+        st.metric("PPG:", ppg_player, rank_ppg_player, "off")
+    midcol1, midcol2, midcol3, midcol4 = st.columns(4)
+    with midcol1:
+        st.metric("QB:", ppg_qb, rank_ppg_qb, "off")
+    with midcol2:    
+        st.metric("RB:", ppg_rb, rank_ppg_rb, "off")
+    with midcol3:
+        st.metric("WR:", ppg_wr, rank_ppg_wr, "off")
+    with midcol4:
+        st.metric("TE:", ppg_te, rank_ppg_te, "off")
+    st.dataframe(stat_table, hide_index=True, use_container_width=True)
